dd6385f2b2207f2e0de50e119250d776f21eda5c
AngieYYF <yuanay@student.unimelb.edu.au>
2023-09-20 01:02:35 +1000

    firebase ci

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDf--XeJ2-pkwKkjGO1RLxzjwzJZUy_e0s",
+    authDomain: "planimation-staging-181bc.firebaseapp.com",
+    projectId: "planimation-staging-181bc",
+    storageBucket: "planimation-staging-181bc.appspot.com",
+    messagingSenderId: "914707935474",
+    appId: "1:914707935474:web:ba4d0f22fa93687482206b",
+    measurementId: "G-XYNE4FJ1CF",
+    databaseURL: 'http://localhost:9150', // Firestore Emulator URL
+};
